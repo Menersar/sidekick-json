6d3cefe5d5c9f82bc95bf00740c76ab2ed5aa823
Menersar <menersar@menersar.com>
2023-08-06 23:58:48 +0200

    update files

--- rollup.config.js
+++ rollup.config.js
@@ -5,3 +5,15 @@ export default {
     format: 'cjs'
   }
 };
+
+
+// export default {
+//     input: {
+//         main: resolve(__dirname, './src/index.js'),
+//     },
+//     output: {
+//       file: './lib/cjs.cjs',
+//       format: 'cjs'
+//     }
+//   };
+  
